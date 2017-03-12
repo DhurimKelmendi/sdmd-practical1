497f322044120e1843a1d29f459e0cfcffa8d981
Dhurim <dhkelmendi@gmail.com>
2017-03-12 20:46:36 +0200

    Add LanguageActivity

--- app/src/main/java/com/example/dhurim/sdmdpractical1/MainActivity.java
+++ app/src/main/java/com/example/dhurim/sdmdpractical1/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 new AdapterView.OnItemClickListener(){
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        //TODO(1) Go to the next Activity, passing the language selected
                         String language = String.valueOf(parent.getItemAtPosition(position));
                         Toast.makeText(MainActivity.this, language, Toast.LENGTH_LONG).show();
                     }
