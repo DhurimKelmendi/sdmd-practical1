994b03f10880ba2ce264639020853af8f225ce2c
Dhurim <dhkelmendi@gmail.com>
2017-03-12 20:39:09 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/dhurim/sdmdpractical1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.dhurim.sdmdpractical1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] languages = {"Albanian", "German"};
+        ListAdapter languageAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, languages);
+        ListView languageListView = (ListView) findViewById(R.id.language_list);
+        languageListView.setAdapter(languageAdapter);
+
+        languageListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener(){
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String language = String.valueOf(parent.getItemAtPosition(position));
+                        Toast.makeText(MainActivity.this, language, Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+    }
+}
